7602071334d8fd48eac7cc27703a2d6de092c440
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2024-06-23 21:51:45 +0545

    Add files via upload

---
+++ index.php
@@ -0,0 +1,5 @@
+echo"<td>
+    "<a href='edit-ledger.php?ledger_id=".$row['id']."><button>Edit</button></a>
+    "<a href='./services/ledgerServices/deleteledger.php?ledger_id=".$row['id']."><button>Delete</button></a>
+  
+</td>
